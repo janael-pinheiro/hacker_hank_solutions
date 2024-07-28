479cd92ffd563563999523a3f2df44a897cd8b1f
AntonioJanael <66491183+AntonioJanael@users.noreply.github.com>
2024-07-28 16:23:36 -0300

    improve grid search performance

--- hacker_hank/grid_search.py
+++ hacker_hank/grid_search.py
@@ -1,24 +1,26 @@
 
 def grid_search(G, P) -> str:
-    response = "NO"
-    current_row_pattern_index = 0
+    first_row_pattern_index = 0
     current_row_grid_index = 0
     current_column_grid_index = 0
     hashed_combined_pattern = hash("".join(P))
-    while current_row_grid_index + len(P) < len(G):
-        length_row_pattern = len(P[current_row_pattern_index])
+    hashed_first_pattern_row = hash(P[first_row_pattern_index])
+    length_row_pattern = len(P[first_row_pattern_index])
+    while current_row_grid_index + len(P) <= len(G):
         length_row_grid = len(G[current_row_grid_index])
         while current_column_grid_index + length_row_pattern <= length_row_grid:
-            combined_grid = ""
-            count = 0
-            row_index = current_row_grid_index
-            while count < len(P):
-                combined_grid += G[row_index][current_column_grid_index:current_column_grid_index + len(P[count])]
-                count += 1
-                row_index += 1
-            if hash(combined_grid) == hashed_combined_pattern:
-                return "YES"
+            current = G[current_row_grid_index][current_column_grid_index:current_column_grid_index + len(P[first_row_pattern_index])]
+            if hash(current) == hashed_first_pattern_row:
+                combined_grid = current
+                count = 1
+                row_index = current_row_grid_index + 1
+                while count < len(P):
+                    combined_grid += G[row_index][current_column_grid_index:current_column_grid_index + len(P[count])]
+                    count += 1
+                    row_index += 1
+                if hash(combined_grid) == hashed_combined_pattern:
+                    return "YES"
             current_column_grid_index += 1
         current_column_grid_index = 0
         current_row_grid_index += 1
-    return response
+    return "NO"
