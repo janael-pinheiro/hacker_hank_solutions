8d17e3b661bbd3ca26b55185f9e994bf4b0e7ae0
AntonioJanael <66491183+AntonioJanael@users.noreply.github.com>
2024-08-15 21:25:17 -0300

    update chocolate feast

--- hacker_hank/chocolate_feast.py
+++ hacker_hank/chocolate_feast.py
@@ -1,1 +1,12 @@
-def 
+def chocolate_feast(n, c, m) -> int:
+    bought = n // c
+    total = bought
+    if bought % m != 0:
+        total += bought // m
+        total += ((bought // m) + (bought % m)) // m
+    else:
+        remain = bought
+        while remain // m > 0:
+            total += (remain // m)
+            remain = (remain // m) + (remain % m)
+    return total
